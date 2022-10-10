347b27b2a36c4d84256218b8ead862153c771bb7
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-10-10 12:53:59 -0300

    Logo, user icon e header!

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 
 export default() => {
 
   const [MovieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(()=>{
     const loadAll = async() => {
@@ -33,8 +35,26 @@ export default() => {
     loadAll()
   }, []);
 
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      }else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
   return(
     <div className = "page">
+
+      <Header black={blackHeader} />
 
       {featuredData &&
         <FeaturedMovie item={featuredData} />
